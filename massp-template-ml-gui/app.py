494323c8ee9ad2e4ec3dffb16d8398bf7b828645
Nguyen Nhat Quang <39874396+MRSNOO@users.noreply.github.com>
2019-07-11 15:36:35 +0700

    Add files via upload

---
+++ massp-template-ml-gui/app.py
@@ -0,0 +1,52 @@
+
+from flask import Flask, request, render_template, url_for, redirect, send_from_directory
+import os
+
+from mpl_toolkits.axes_grid1 import host_axes
+
+import config
+import time
+import cv2
+import numpy as np
+from haze_removal import haze_removal
+import try_lib
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', name='hieu pro', img=None)
+
+
+@app.route('/img_show/<path:filename>')
+def img_show(filename):
+    return send_from_directory(config.img_out_dir, filename)
+
+@app.route("/handleUpload", methods=['POST'])
+def handle_file_upload():
+    print('received')
+    print(request.files)
+    new_name="xxx"
+    if 'photo' in request.files:
+        photo = request.files['photo']
+        if photo.filename != '':
+            new_name = str(int(time.time())) + '.jpg'
+            photo.save(os.path.join(config.img_dir, new_name))
+            # time.sleep(1)
+            de_haze(new_name) # solve problem
+            time.sleep(1)
+        else:
+            print('nothoing')
+    else:
+        print('no file')
+    return new_name
+    # return redirect(url_for('/home', filename=new_name))
+
+
+def de_haze(new_name):
+    image = np.array(cv2.imread(config.img_dir + new_name))
+    cv2.imwrite(config.img_out_dir + new_name, haze_removal(image))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
---
+++ massp-template-ml-gui/config.py
@@ -0,0 +1,4 @@
+import os
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+img_dir = ROOT_DIR + '/static/input/'
+img_out_dir = ROOT_DIR + '/static/output/'
